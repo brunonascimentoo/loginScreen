e2b777d907f4b83883eb62469c922c3a0d98900f
Bruno Nascimento <bnascimentos1996@gmail.com>
2022-10-20 23:12:27 -0300

    Remoçao do fragment do componente Screen

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -3,24 +3,22 @@ import "./screen.css";
 
 export function Screen() {
   return (
-    <>
-      <div className="screen-container">
-        <div className="screen-card-container">
-          <div className="screen-login-name">
-            <h1>Login</h1>
-            <p>Enter yuor credentials</p>
-          </div>
+    <div className="screen-container">
+      <div className="screen-card-container">
+        <div className="screen-login-name">
+          <h1>Login</h1>
+          <p>Enter yuor credentials</p>
+        </div>
 
-          <div className="screen-input-container">
-            <input type="text" placeholder="Enter with your email" />
-            <input type="password" placeholder="Enter with your password" />
-            <button type="submit">login</button>
-            <a href="#">
-              <p>Forgot yout password?</p>
-            </a>
-          </div>
+        <div className="screen-input-container">
+          <input type="text" placeholder="Enter with your email" />
+          <input type="password" placeholder="Enter with your password" />
+          <button type="submit">login</button>
+          <a href="#">
+            <p>Forgot yout password?</p>
+          </a>
         </div>
       </div>
-    </>
+    </div>
   );
 }
